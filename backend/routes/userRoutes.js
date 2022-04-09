053ba6649f8a5ae809477a594cf18ca208f330a3
anilkumar7838 <anilkumar9868573282@gmail.com>
2022-04-09 18:45:39 +0530

    backend s-1

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logout,
+  getUserDetails,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+} = require("../controllers/userController");
+const {
+  isAuthenticatedUser,
+} = require("../middleware/authentication");
+
+const router = express.Router();
+
+router.route("/signup").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:Token").put(resetPassword);
+router.route("/logout").get(logout);
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+module.exports = router;
